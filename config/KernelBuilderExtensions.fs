f1a3bfa3187cc06d9659df508bb6181bdc6f35de
Andrea Tupini <tupini07@gmail.com>
2023-06-07 17:26:55 -0600

    chat completion service is messy

--- config/KernelBuilderExtensions.fs
+++ config/KernelBuilderExtensions.fs
@@ -5,16 +5,15 @@ open KernelSettings
 
 type KernelBuilder with
     member x.WithCompletionService(settings: KernelSettings) =
-        x
-            .WithOpenAITextCompletionService(
-                modelId = settings.DeploymentOrModelId,
-                apiKey = settings.ApiKey,
-                orgId = "",
-                serviceId = settings.ServiceId
-            )
-            .WithOpenAIChatCompletionService(
-                modelId = settings.DeploymentOrModelId,
-                apiKey = settings.ApiKey,
-                orgId = "",
-                serviceId = settings.ServiceId
-            )
+        x.WithOpenAITextCompletionService(
+            modelId = settings.DeploymentOrModelId,
+            apiKey = settings.ApiKey,
+            orgId = "",
+            serviceId = settings.ServiceId
+        )
+// .WithOpenAIChatCompletionService(
+//     modelId = settings.DeploymentOrModelId,
+//     apiKey = settings.ApiKey,
+//     orgId = "",
+//     serviceId = settings.ServiceId
+// )
