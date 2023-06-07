d535f6c3dac0320a4004ae82298836713a6295b7
Andrea Tupini <tupini07@gmail.com>
2023-06-07 16:59:16 -0600

    add both chat and completion services

--- config/KernelBuilderExtensions.fs
+++ config/KernelBuilderExtensions.fs
@@ -5,21 +5,16 @@ open KernelSettings
 
 type KernelBuilder with
     member x.WithCompletionService(settings: KernelSettings) =
-        ignore
-        <| match settings.EndpointType with
-           | TextCompletion ->
-               x.WithOpenAITextCompletionService(
-                   modelId = settings.DeploymentOrModelId,
-                   apiKey = settings.ApiKey,
-                   orgId = "",
-                   serviceId = settings.ServiceId
-               )
-           | ChatCompletion ->
-               x.WithOpenAIChatCompletionService(
-                   modelId = settings.DeploymentOrModelId,
-                   apiKey = settings.ApiKey,
-                   orgId = "",
-                   serviceId = settings.ServiceId
-               )
-
         x
+            .WithOpenAITextCompletionService(
+                modelId = settings.DeploymentOrModelId,
+                apiKey = settings.ApiKey,
+                orgId = "",
+                serviceId = settings.ServiceId
+            )
+            .WithOpenAIChatCompletionService(
+                modelId = settings.DeploymentOrModelId,
+                apiKey = settings.ApiKey,
+                orgId = "",
+                serviceId = settings.ServiceId
+            )
